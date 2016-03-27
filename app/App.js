bb3c1a039ebad91f86619377c35bd1f3dc8c5293
Jaakko Alajoki <jaamo@Jaakkos-MacBook-Pro.local>
2016-03-27 11:28:50 +0300

    Added search funcionality.

--- app/App.js
+++ app/App.js
@@ -3,52 +3,64 @@ var Store = require('./Store.js');
 var actions = require('./actions.js');
 
 var App = React.createClass({
+
   getInitialState: function () {
     return {
       messages: Store.getMessages(),
-      newMessage: ''
+      keyword: ''
     };
   },
+
   componentWillMount: function () {
     Store.addChangeListener(this.changeState);
   },
+
   componentWillUnmount: function () {
     Store.removeChangeListener(this.changeState);
   },
+
   changeState: function () {
     this.setState({
       messages: Store.getMessages()
     });
   },
-  addMessage: function (event) {
+
+  searchMessages: function (event) {
     event.preventDefault();
-    var input = this.refs.newMessage.getDOMNode();
-    actions.addMessage(input.value);
+    var input = this.refs.keyword;
+    actions.searchMessages(input.value);
+    console.log("Start search.");
     this.setState({
-      newMessage: ''
+      keyword: ''
     });
   },
-  updateNewMessage: function (event) {
+
+  updateKeyword: function (event) {
+    console.log("update kw");
     this.setState({
-      newMessage: event.target.value
+      keyword: event.target.value
     });
   },
+
   renderMessages: function (message) {
     return (
-      <div>{message}</div>
+      <div>{message[0]}: {message[1]}</div>
     );
   },
+
 	render: function() {
+
 		return (
 			<div>
         {this.state.messages.map(this.renderMessages)}
-        <form onSubmit={this.addMessage}>
-          <input ref="newMessage" type="text" value={this.state.newMessage} onChange={this.updateNewMessage}/>
+        <form onSubmit={this.searchMessages}>
+          <input ref="keyword" type="text" value={this.state.keyword} onChange={this.updateKeyword}/>
         </form>
       </div>
 		);
+
 	}
-	
+
 });
-	
+
 module.exports = App;
--- app/Store.js
+++ app/Store.js
@@ -2,17 +2,49 @@ var flux = require('flux-react');
 var actions = require('./actions.js');
 
 module.exports = flux.createStore({
-  messages: [],
-  actions: [
-    actions.addMessage
+
+  // The dictionary.
+  messages: [
+    ["ohjaustangonkannatin", "stem"],
+    ["satula", "saddle"]
   ],
-  addMessage: function (message) {
-    this.messages.push(message);
+
+  // Filtered dictionary.
+  filteredMessages: [
+
+  ],
+
+  // Available actions.
+  actions: [
+    actions.searchMessages
+  ],
+
+  /**
+   * Search dictionary by given keyword.
+   * @param  {[type]} keyword [description]
+   * @return {[type]}         [description]
+   */
+  searchMessages: function(keyword) {
+
+    console.log("Search with keyword: " + keyword);
+
+    // Clean the result list.
+    this.filteredMessages = [];
+
+    // Filter original list by the keyword.
+    for (var i = 0; i < this.messages.length; i++) {
+      if (this.messages[i][0].indexOf(keyword) != -1 || this.messages[i][1].indexOf(keyword) != -1) {
+        this.filteredMessages.push(this.messages[i]);
+      }
+    }
+
     this.emitChange();
+
   },
+
   exports: {
     getMessages: function () {
-      return this.messages;
+      return this.filteredMessages;
     }
   }
 });
